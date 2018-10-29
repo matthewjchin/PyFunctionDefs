50e3b541bc8a154a4ed0e96ec89ea93fa68291ac
Matthew Chin <mattjchin@Matthews-MacBook-Pro.local>
2018-10-29 15:17:26 -0700

    First Commit

---
+++ hotdog.py
@@ -0,0 +1,75 @@
+# ----- HOTDOG.PY -----
+# Author: Matthew Chin
+# GitHub: https://github.com/mattjchin
+#
+# This program features the use of functions aside from a main function that
+# also takes in user input as the program begins from the main function.
+# Mathematical operations in use for this program include modulus, addition,
+# division, and subtraction. The functions also allow for user to be returned
+# output as a result of the use of functions, to which a return value is
+# required in the Python language.
+
+# Function to find the number of packages of dogs needed for cookout
+# Packages of hot dogs come in 10
+# Determine how many are needed
+def packagesNeeded(numItems, itemsPerPackage):
+    if numItems % itemsPerPackage == 0:
+        # packsNec will return as a float, in print statement
+        # packsNec will return as a string
+        packsNec = numItems / itemsPerPackage
+        return packsNec
+    else:
+        # find integer division of packages
+        packsNec = numItems // itemsPerPackage
+        # add 1 when there are remaining dogs or buns
+        packsNec = packsNec + 1
+        return packsNec
+
+
+# From the packages of hot dogs used find how many individual dogs are remainders
+# Only if applicable - if otherwise then the value of 0 is returned
+def itemsLeftover(numItems, itemsPerPackage):
+    remgDogs = numItems % itemsPerPackage
+    if remgDogs != 0:
+        # subtract the remainder of remgDogs from total number of objects in a package
+        remgDogs = itemsPerPackage - remgDogs
+        return remgDogs
+    else:
+        remgDogs = 0
+        return remgDogs
+
+
+# User input taken to find number of hot dogs and number of people attending cookout
+# Input all currently string type, converted to int (integer) type after input taken
+def main():
+    people = int(input("How many people will be at the cookout? "))
+    hotDogs = int(input("How many hot dogs will each person be given? "))
+
+    # total number of dogs cooked = people attending * hot dogs given per person
+    hotDogs = people * hotDogs
+
+    # number of hot dog packages required
+    # hot dog packages in 10 per pack
+    # dogsPerPack = 10
+    print("There are", str(int(packagesNeeded(hotDogs, 10))), "packages of hot dogs needed.")
+    # if no remainder by dividing the function will return and print a float - turn into string
+
+    # number of hot dog buns required
+    # hot dog buns in 8 per pack
+    # bunsPerPack = 8
+    print("There are", str(int(packagesNeeded(hotDogs, 8))), "packages of hot dog buns needed. ")
+    # if no remainder by dividing the function will return and print a float - turn into string
+
+    # remaining hot dogs
+    # hot dog packages in 10 per pack
+    # dogsPerPack = 10
+    print("There are", str(itemsLeftover(hotDogs, 10)), "hot dogs remaining.")
+
+    # remaining hot dog buns
+    # hot dog buns in 8 per pack
+    # bunsPerPack = 8
+    print("There are", str(itemsLeftover(hotDogs, 8)), "hot dog buns remaining.")
+
+
+# Call the main function
+main()
---
+++ leapyear.py
@@ -0,0 +1,55 @@
+# ----- LEAPYEAR.PY -----
+# Author: Matthew Chin
+# GitHub: https://github.com/mattjchin
+#
+# This program features the use of functions aside from a main function that
+# also takes in user input as the program begins from the main function.
+# Mathematical operations in use for this program include modulus, addition,
+# division, and subtraction. The functions also allow for user to be returned
+# output as a result of the use of functions, to which a return value is
+# required in the Python language.
+#
+# This program checks if a year that the user inputs is a leap year or not.
+# A leap year is 366 days, different from the 365 days in a calendar year.
+# This is determined if the year inputted is divisible by 4, 100, and 400,
+# which is a leap year that starts from the year 0 and on. Therefore, the
+# years 2000, 2004, 2008, 2012, and 2016 are leap years, but 1900, 1904,
+# and 1908 are not leap years even though the former years listed are even.
+# Just because a year is even does not mean that the year is a leap year.
+# If an inputted year is NOT divisible by 4, 100, and 400, the year that
+# is inputted by the user is NOT a leap year and will return a statement
+# of boolean type: True if it is, False if otherwise.
+# The modulus operand was used for this program to check divisibility as if
+# whether or not there are remainders in each of the statements that the
+# argument is checked. If all three statements are met such that each modulus
+# equation has a value of 0, it is a leap year
+
+# Function used to determine if a year is a leap year
+# Modulus checked to see if there are any remainders
+# If remainder exists then it is
+# Year must have modulus or remainder value of 0 when
+# checked to be divisible by 4, 100, and 400 to return True
+# False returned otherwise
+def isLeapYear(year):
+    if year%4==0:
+        if year%100==0:
+            if(year%400==0):
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+# Main function created for taking user input
+# Calls the isLeapYear function with the year that was user input converted into int type
+def main():
+	print("You will input a year and will find if it is a leap year. ")
+	userYear = int(input("Please enter a year with at least one digit: "))
+	yearDetd = isLeapYear(userYear)
+	print(yearDetd)
+
+# Call the main function
+main()
